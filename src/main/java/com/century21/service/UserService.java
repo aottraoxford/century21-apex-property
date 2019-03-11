2545c4480e6167157bd8979d090244116ce12811
unknown <aottraoxford@gmail.com>
2019-03-11 08:58:27 +0700

    aaa

--- src/main/java/com/century21/service/UserService.java
+++ src/main/java/com/century21/service/UserService.java
@@ -3,6 +3,6 @@ package com.century21.service;
 import com.century21.repository.UserRepo;
 
 public interface UserService {
-//    void sendMail(String email);
-//    void changePassword(UserRepo.ChangePassword changePassword);
+    void sendMail(String email);
+    void changePassword(UserRepo.ChangePassword changePassword);
 }
