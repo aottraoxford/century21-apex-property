54c03d11386fc3344754a2b9d528aa1e57113a77
unknown <aottraoxford@gmail.com>
2019-03-18 16:45:58 +0700

    aaa

--- src/main/java/com/century21/repository/ProjectRepo.java
+++ src/main/java/com/century21/repository/ProjectRepo.java
@@ -269,7 +269,7 @@ public interface ProjectRepo {
                     if(filter.getToPrice()>0 && filter.getFromPrice()>0)
                         WHERE("price between #{filter.fromPrice} AND #{filter.toPrice}");
                     else if(filter.getToPrice()>0) WHERE("price < #{filter.toPrice}");
-                    else if(filter.getFromPrice()>0) WHERE("price <")
+                    else if(filter.getFromPrice()>0) WHERE("price <");
                     if(filter.getRentOrBuy()!=null && filter.getRentOrBuy().length()>0)
                         WHERE("rent_or_buy ILIKE #{filter.rentOrBuy}");
                     if(filter.getSortType()!=null && filter.getSortType().length()>0 ){
